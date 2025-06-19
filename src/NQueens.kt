b424fd03b072e13a876d543f5418a5e6e22df35e
wybrand <wybrandvdmeer@gmail.com>
2025-06-19 19:10:59 +0200

    jj

--- src/NQueens.kt
+++ src/NQueens.kt
@@ -2,69 +2,112 @@ import java.lang.System.exit
 import kotlin.math.pow
 
 fun main() {
-    val size = 8;
-    val nQueens = NQueens();
-    var bitmask = nQueens.queen(10, size);
-    nQueens.print(bitmask, size);
+    val nQueens = NQueens(5);
+    nQueens.algo();
 }
 
-class NQueens {
+class NQueens(val size: Int) {
 
-    fun queen(fieldNo: Int, n: Int): Long {
-        return bishop(fieldNo, n) or rook(fieldNo, n);
+    fun algo() {
+        for(y in 0 until size) {
+            var fieldNo = toFieldNo(0, y);
+            var bitmask : Long = queen(fieldNo);
+
+            var notCompleted = false;
+            for(attempt in 0 until size - 1) {
+                fieldNo = nextQueen(bitmask, fieldNo);
+                if(fieldNo == -1) {
+                    notCompleted = true;
+                    break;
+                }
+                bitmask = bitmask or queen(fieldNo);
+            }
+
+            if(!notCompleted) {
+                System.out.printf("Found combination.\n")
+            }
+        }
+    }
+    fun nextQueen(bitmask : Long, fieldNo : Int) : Int {
+        val x = fieldNo%size;
+
+        for(nextY in 0 until size) {
+            val nxtFieldNo = toFieldNo(x + 1, nextY);
+
+            if(nxtFieldNo >= size * size) {
+                return -1;
+            }
+
+            if(!capture(bitmask, nxtFieldNo)) {
+                return nxtFieldNo;
+            }
+        }
+        return -1;
     }
 
-    fun bishop(fieldNo: Int, n: Int): Long {
-        val y = fieldNo/n;
+    fun toFieldNo(x : Int, y : Int) : Int {
+        return y * size + x;
+    }
+
+    fun queen(fieldNo: Int): Long {
+        return bishop(fieldNo) or rook(fieldNo);
+    }
+
+    fun bishop(fieldNo: Int): Long {
+        val y = fieldNo/size;
 
         var bitmask : Long = 0;
 
-        for(idx in 0 until n) {
+        for(idx in 0 until size) {
             // Forward diagonal.
-            var bit = fieldNo + (idx - y) * (n+1);
-            if(bit >= idx * n && bit < idx * n + n) {
+            var bit = fieldNo + (idx - y) * (size+1);
+            if(bit >= idx * size && bit < idx * size + size) {
                 bitmask = bitmask or 2.0.pow(bit).toLong();
             }
 
             // Backward diagonal.
-            bit = fieldNo + (idx - y)  * (n-1);
-            if(bit >= idx * n && bit < idx * n + n) {
+            bit = fieldNo + (idx - y)  * (size-1);
+            if(bit >= idx * size && bit < idx * size + size) {
                 bitmask = bitmask or 2.0.pow(bit).toLong();
             }
         }
         return bitmask;
     }
 
-    fun rook(fieldNo: Int, n: Int): Long {
-        val x = (fieldNo%n);
-        val y = fieldNo/n;
+    fun rook(fieldNo: Int): Long {
+        val x = fieldNo%size;
+        val y = fieldNo/size;
 
         var bitmask : Long = 0;
         var shiftValue : Long = 0;
 
-        for(idx in 0 until n) {
+        for(idx in 0 until size) {
             shiftValue += 2.0.pow(idx).toLong();
         }
 
-        bitmask = bitmask or (shiftValue shl (y*n));
+        bitmask = bitmask or (shiftValue shl (y*size));
 
-        for(idx in 0 until n) {
-            bitmask = bitmask or 2.0.pow(x + idx * n).toLong();
+        for(idx in 0 until size) {
+            bitmask = bitmask or 2.0.pow(x + idx * size).toLong();
         }
 
         return bitmask;
     }
 
-    fun print(bitmask: Long, size: Int) {
+    fun capture(bitmask : Long, fieldNo: Int) : Boolean {
+        return (bitmask and (1L shl fieldNo)) shr fieldNo == 1L;
+    }
+
+    fun print(bitmask: Long) {
         System.out.printf("\n");
         var y = 0;
         for(idx in 0 until size * size) {
-            val value : Long = (bitmask and (1L shl idx)) shr idx;
+            val dot : Boolean = capture(bitmask, idx);
             if(y != idx/size) {
                 System.out.printf("\n");
                 y = idx/size;
             }
-            System.out.printf("%c ", if(value == 1L) 'x' else '.');
+            System.out.printf("%c ", if(dot) 'x' else '.');
         }
         System.out.printf("\n");
     }
